1b6a634a7163be96df060cdc691089dda195bc47
chengh <821635867@qq.com>
2022-03-20 14:38:04 +0800

    Initial commit

---
+++ src/main/java/com/chengh/spingmvc/SpingMvcApplication.java
@@ -0,0 +1,13 @@
+package com.chengh.spingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingMvcApplication.class, args);
+    }
+
+}
